f3b1784c52c3583f34202e5e5d010a687d48846f
clonebidon <glebgayda7@gmail.com>
2025-01-13 14:04:03 +0200

    main update

---
+++ dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let message = "блять как с тайпскриптом";
+function add(x, y) {
+    return x + y;
+}
+// Пример использования
+console.log(message);
+console.log(`2 + 3 = ${add(2, 3)}`);
+// Использование интерфейса
+const user = {
+    name: "глеб",
+    age: 14
+};
+console.log(`Пользователь: ${user.name}, возраст: ${user.age}`);
